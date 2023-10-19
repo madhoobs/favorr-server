57605db5cd23593bee8904e2903eaed70a9a7e98
Hasan Almadhoob <hasan@madhoob.com>
2023-10-19 12:31:52 +0300

    Add User Ctrl, Route & Middleware

--- models/User.js
+++ models/User.js
@@ -2,14 +2,15 @@ const { Schema } = require('mongoose')
 
 const userSchema = new Schema(
   {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    passwordDigest: { type: String, required: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    email: { type: String, required: true },
-    passwordDigest: { type: String, required: true },
-    profilePicture: { type: String }
-    // favors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Favor' }],
-    // orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-    // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+    profilePicture: { type: String },
+    favors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Favor' }],
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
   },
   { timestamps: true }
 )
