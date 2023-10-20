4513569c92c7604f95fea3eafc83af7a8a00c703
Hasan Almadhoob <hasan@madhoob.com>
2023-10-21 02:37:57 +0300

    Fix routers

--- server.js
+++ server.js
@@ -3,32 +3,35 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
-// Import the routes
-const AuthRouter = require('./routes/AuthRoutes')
-const CategoryRoute = require('./routes/CategoryRoute')
-const FavorRoute = require('./routes/FavorRoute')
-const PackageRoute = require('./routes/PackageRoute')
-const CommentRouter = require('./routes/CommentRoutes')
 // Port configurations
 const PORT = process.env.PORT
-// Receive the mongoDB connection
+
+// Receive MongoDB connection
 const db = require('./db')
 
 // Invoke express functionality
 const app = express()
-// middleware
+
+// Middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// Mount the routes
+// Import routes
+const AuthRouter = require('./routes/AuthRouter')
+const CategoryRouter = require('./routes/CategoryRouter')
+const FavorRouter = require('./routes/FavorRouter')
+const PackageRouter = require('./routes/PackageRouter')
+const CommentRouter = require('./routes/CommentRouter')
+
+// Mount routes
 app.use('/', AuthRouter)
-app.use('/category', CategoryRoute)
-app.use('/favor', FavorRoute)
-app.use('/package', PackageRoute)
+app.use('/category', CategoryRouter)
+app.use('/favor', FavorRouter)
+app.use('/package', PackageRouter)
 app.use('/', CommentRouter)
 
 // Listen to requests on port
 app.listen(PORT, () => {
-  console.log(`Favorr running on Port ${PORT}`)
+  console.log(`Favorr server is running on port ${PORT}`)
 })
